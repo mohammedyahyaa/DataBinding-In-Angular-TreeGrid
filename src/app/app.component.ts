c3d31de656c4c7300c1d9c36adb558240000ad0f
mohammedyahyaa <my212634@gmail.comd>
2022-05-19 19:40:21 +0200

    commit Data Binding in Angular TreeGrid component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import SampleData from './files/user.json';
+import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
+import { TreeGridComponent } from '@syncfusion/ej2-angular-treegrid';
+import {trigger, state,style,animate,transition} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //animation
+  animations: [
+    trigger('fade', [
+      transition('void <=> *',[
+        style({backgroundColor : 'greenYellow' , opacity :0 }),
+        animate(3000)
+      ] )
+    ])
+    
+  ]
 })
-export class AppComponent {
-  title = 'User_data_task';
+
+
+
+export class AppComponent implements OnInit {
+
+  public data: Object[] | undefined;
+  public pageSettings: PageSettingsModel | undefined;
+
+  ngOnInit(): void {
+    this.data = SampleData;
+    this.pageSettings = { pageSize: 6 };
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule,ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
+import { PageService, SortService, FilterService } from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    TreeGridModule,
+    ButtonModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [PageService,
+    SortService,
+    FilterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
